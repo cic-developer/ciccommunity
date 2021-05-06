f42d2923bfe7e1b3422780cb09c59862b1dbb2dc
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-06 10:06:01 +0000

    2021-05-10:05:01 auto commit

--- views/mypage/cic/withdraw.php
+++ views/mypage/cic/withdraw.php
@@ -56,9 +56,9 @@
                     ?>
                         <tr>
                             <td><?php echo html_escape(element('wid_req_datetime', $result)); ?></td>
-                            <td>200,000 CP</td>
-                            <td>100 PER</td>
-                            <td>fasdfsa68bn4a3hg7h4a3ba6ava99</td>
+                            <td><?php echo number_format(element('wid_req_money', $result), 2); ?>CP</td>
+                            <td><?php echo number_format(element('wid_percoin', $result), 2); ?> PER</td>
+                            <td><?php echo html_escape(element('wid_transaction', $result)); ?></td>
                             <td>
                                 <p class="cblue">승인</p>
                             </td>
