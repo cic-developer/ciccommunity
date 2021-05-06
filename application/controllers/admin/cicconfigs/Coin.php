b6e42e78c2c05bd41e466e7198ddb612d9ff610e
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-06 08:10:01 +0000

    2021-05-08:05:01 auto commit

--- application/controllers/admin/cicconfigs/Coin.php
+++ application/controllers/admin/cicconfigs/Coin.php
@@ -112,14 +112,9 @@ class Coin extends CB_Controller
 			if(isset($list) && !empty($list)){
 				$stock = $this->Coin_model->dropdown_list($list);
 			}	
-			$view['view']['alert_message'] = '정상적으로 저장되었습니다';}
+			$view['view']['alert_message'] = '정상적으로 저장되었습니다';
 			$view['view']['event']['formruntrue'] = Events::trigger('formruntrue', $eventname);
-		
-		
-		
 		}
-
-
 
 
        //Get market data 
