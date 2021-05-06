0d0080f1c1163e42e9f605571629ba8bba94a55c
rimose <yop-ko@nate.com>
2021-05-06 07:38:01 +0000

    2021-05-07:05:01 auto commit

--- views/admin/basic/withdraw/withdraws/index.php
+++ views/admin/basic/withdraw/withdraws/index.php
@@ -127,11 +127,11 @@
                                                 <input type="hidden" name="wid_idx1" id="wid_idx1" value="" />
                                                 <div class="form-group">
                                                     <label for="cp_transaction">트랜잭션:</label>
-                                                    <textarea class="form-control" rows="5" cols="75" id="cp_transaction" name="cp_transaction" placeholder="출금 승인 사유를 입력해주세요." style="width:100%;"></textarea>
+                                                    <textarea class="form-control" rows="1" cols="75" id="cp_transaction" name="cp_transaction" placeholder="트랜잭션을 입력해주세요." style="width:100%;"></textarea>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="cp_percoin">퍼코인:</label>
-                                                    <textarea class="form-control" rows="5" cols="75" id="cp_percoin" name="cp_percoin" placeholder="출금 승인 사유를 입력해주세요." style="width:100%;"></textarea>
+                                                    <textarea class="form-control" rows="1" cols="75" id="cp_percoin" name="cp_percoin" placeholder="퍼코인을 입력해주세요." style="width:100%;"></textarea>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="cp_content1">사유:</label>
