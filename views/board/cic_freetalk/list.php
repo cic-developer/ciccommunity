d956d1eb04d3d1bbf3a6e79f47caca978b75cfa1
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-07 07:43:01 +0000

    2021-05-07:05:01 auto commit

--- views/board/cic_freetalk/list.php
+++ views/board/cic_freetalk/list.php
@@ -36,13 +36,8 @@
                     </ul>
                 </div>
                 <div class="fr">
-                    
                     <h4>실시간 인기 게시물</h4>
                     <ul>
-                    						<?php
-						if (element('list', element('data', $view))) {
-							foreach (element('list', element('data', $view)) as $result) {
-						?>
                         <!-- <li><a href="<?php echo base_url('post/5')?>">1. [스압] 월급루팡이 만들어지는 과정 (5) <span>28</span></a>
                         </li>
                         <li><a href="<?php echo base_url('post/5')?>">2. [스압] 월급루팡이 만들어지는 과정 (5) <span>28</span></a>
@@ -61,8 +56,8 @@
                         </li>
                         <li><a href="<?php echo base_url('post/5')?>">9. [스압] 월급루팡이 만들어지는 과정 (5) <span>28</span></a>
                         </li>
-                        <li><a href="<?php echo base_url('post/5')?>">10. [스압] 월급루팡이 만들어지는 과정 (5) <span>28</span></a>
-                        </li> -->
+                        <li><a href="<?php echo base_url('post/5')?>">10. [스압] 월급루팡이 만들어지는 과정 (5) <span>28</span></a> -->
+                        </li>
                     </ul>
                 </div>
             </div>
