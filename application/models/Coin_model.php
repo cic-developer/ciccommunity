79b09fc746025407d68226f646613e7077779ef6
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-07 05:40:01 +0000

    2021-05-05:05:01 auto commit

--- application/models/Coin_model.php
+++ application/models/Coin_model.php
@@ -166,8 +166,8 @@ class Coin_model extends CB_Model
         $array = json_decode($response, true);
 
        // $data = (object)array($array);
-        foreach($array as $kwr_stocks){
-			$kwr_market = $kwr_stocks[0]['market'];
+        foreach($array as $kwr_stocks => $value){
+			$kwr_market = $value['market'];
             print_r($kwr_market);
 			if(strcmp(substr($kwr_market, 0, 1), "K")==0){
                 return $kwr_stocks;
