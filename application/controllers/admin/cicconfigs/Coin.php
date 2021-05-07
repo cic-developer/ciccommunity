d72f4fdff6ee4f1fc88db0edf29db049d19c3639
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-07 09:40:01 +0000

    2021-05-09:05:01 auto commit

--- application/controllers/admin/cicconfigs/Coin.php
+++ application/controllers/admin/cicconfigs/Coin.php
@@ -89,9 +89,9 @@ class Coin extends CB_Controller
 	
 		//GET COIN MARKET INFORMATION FOR DROPDOWN LIST
 
-		$getStock = $this -> Coin_model->getstockData();
-		$view['getStock'] = $getStock;
-        print_r(1);
+		// $getStock = $this -> Coin_model->getstockData();
+		// $view['getStock'] = $getStock;
+        // print_r(1);
 		//CREATE COIN LIST FOR ADMIN
 
 		$this->load->library('form_validation');
