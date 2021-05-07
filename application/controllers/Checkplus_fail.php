5781a6f16048a944064e93ef5aebfbb39452be18
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-07 09:42:01 +0000

    2021-05-09:05:01 auto commit

--- application/controllers/Checkplus_fail.php
+++ application/controllers/Checkplus_fail.php
@@ -45,7 +45,7 @@ class Checkplus_main extends CB_Controller
 	 */
 	public function index()
 	{
-		$this->load->view('register/cic/checkplus_main');
+		$this->load->view('register/cic/Checkplus_fail');
 	}
 }
 
