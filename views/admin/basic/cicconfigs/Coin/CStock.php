56bffaf25cf2384741f9e6b74325846977fe059f
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-10 07:17:01 +0000

    2021-05-07:05:01 auto commit

--- views/admin/basic/cicconfigs/Coin/CStock.php
+++ views/admin/basic/cicconfigs/Coin/CStock.php
@@ -9,7 +9,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<form method='post' action="<?= base_url() ?>/application/controllers/admin/cicconfigs/Coin/loadRecord" >
 			<div class='box-search'>
 				<div class='input-group'>
-					<input type='text' name='search' value='<?= $search ?>'><input type='submit' name='submit' value='검색'>
+					<input type='text' name='search' value='<?= $search ?>'>
+					<span class = "input-group-btn">
+						<button type='submit' class="btn btn-default btn-sm" name='submit' value='검색'>검색!</button>
+					</span>
 				</div>
 			</div>	
 		</form><br/> 
