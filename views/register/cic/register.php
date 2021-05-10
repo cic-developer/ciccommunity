ccebfb6b95cf99699b432626ba84778a8441c0a6
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-10 06:09:01 +0000

    2021-05-06:05:01 auto commit

--- views/register/cic/register.php
+++ views/register/cic/register.php
@@ -269,6 +269,7 @@
 					<p class="chk-check"><input type="checkbox" id="jsel03"><label for="jsel03">개인정보 수집 및 이용에
 							동의합니다.</label></p>
 				</div>
+				<?php echo form_close(); ?>
 				<div class="item c04">
 					<h4>본인확인</h4>
 					<div class="txt">
@@ -288,7 +289,6 @@
 					</div>
 				</div>
 			</div>
-			<?php echo form_close(); ?>
 		</div>
 		<!-- page end // -->
 	</div>
