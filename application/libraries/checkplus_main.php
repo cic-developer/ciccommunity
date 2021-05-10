c1310a01ef5be1e0f4fbfec54d3800c4d272f70d
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-10 04:03:01 +0000

    2021-05-04:05:01 auto commit

--- application/libraries/checkplus_main.php
+++ application/libraries/checkplus_main.php
@@ -10,80 +10,96 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Checkplus_main
 {
 
-	public function filter($ip_list = '', $ip = null)
+	public function getCheckPlus()
 	{
-		if (empty($ip)) {
-			// $ip = $_SERVER['REMOTE_ADDR'];
-			if ($_SERVER['HTTP_CLIENT_IP'])
-				$ip = $_SERVER['HTTP_CLIENT_IP'];
-			else if($_SERVER['HTTP_X_FORWARDED_FOR'])
-				$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-			else if($_SERVER['HTTP_X_FORWARDED'])
-				$ip = $_SERVER['HTTP_X_FORWARDED'];
-			else if($_SERVER['HTTP_FORWARDED_FOR'])
-				$ip = $_SERVER['HTTP_FORWARDED_FOR'];
-			else if($_SERVER['HTTP_FORWARDED'])
-				$ip = $_SERVER['HTTP_FORWARDED'];
-			else if($_SERVER['REMOTE_ADDR'])
-				$ip = $_SERVER['REMOTE_ADDR'];
-			else
-				$ip = 'UNKNOWN';
+		//**************************************************************************************************************
+		//NICE평가정보 Copyright(c) KOREA INFOMATION SERVICE INC. ALL RIGHTS RESERVED
+		
+		//서비스명 :  체크플러스 - 안심본인인증 서비스
+		//페이지명 :  체크플러스 - 메인 호출 페이지
+		
+		//보안을 위해 제공해드리는 샘플페이지는 서비스 적용 후 서버에서 삭제해 주시기 바랍니다.
+		//방화벽 정보 : IP 121.131.196.215 , Port 80, 443     
+		//**************************************************************************************************************
+		
+		session_start();
+		
+		$sitecode = "BU370";			// NICE로부터 부여받은 사이트 코드
+		$sitepasswd = "nHmB3aEoHAiK";			// NICE로부터 부여받은 사이트 패스워드
+		
+		// Linux = /절대경로/ , Window = D:\\절대경로\\ , D:\절대경로\
+		$cb_encode_path = "/home/bitnami/dev_ciccommunity/CPClient_64bit";
+		/*
+		┌ cb_encode_path 변수에 대한 설명  ──────────────────────────────────
+			모듈 경로설정은, '/절대경로/모듈명' 으로 정의해 주셔야 합니다.
+			
+			+ FTP 로 모듈 업로드시 전송형태를 'binary' 로 지정해 주시고, 권한은 755 로 설정해 주세요.
+			
+			+ 절대경로 확인방법
+			1. Telnet 또는 SSH 접속 후, cd 명령어를 이용하여 모듈이 존재하는 곳까지 이동합니다.
+			2. pwd 명령어을 이용하면 절대경로를 확인하실 수 있습니다.
+			3. 확인된 절대경로에 '/모듈명'을 추가로 정의해 주세요.
+		└────────────────────────────────────────────────────────────────────
+		*/
+		
+		$authtype = "";      		// 없으면 기본 선택화면, X: 공인인증서, M: 핸드폰, C: 카드 (1가지만 사용 가능)
+			
+		$popgubun 	= "N";		//Y : 취소버튼 있음 / N : 취소버튼 없음
+		$customize 	= "";		//없으면 기본 웹페이지 / Mobile : 모바일페이지 (default값은 빈값, 환경에 맞는 화면 제공)
+		
+		$gender = "";      		// 없으면 기본 선택화면, 0: 여자, 1: 남자
+		
+		$reqseq = "REQ_0123456789";     // 요청 번호, 이는 성공/실패후에 같은 값으로 되돌려주게 되므로
+										// 업체에서 적절하게 변경하여 쓰거나, 아래와 같이 생성한다.
+										
+		// 실행방법은 싱글쿼터(`) 외에도, 'exec(), system(), shell_exec()' 등등 귀사 정책에 맞게 처리하시기 바랍니다.
+		$reqseq = `$cb_encode_path SEQ $sitecode`;
+		
+		// CheckPlus(본인인증) 처리 후, 결과 데이타를 리턴 받기위해 다음예제와 같이 http부터 입력합니다.
+		// 리턴url은 인증 전 인증페이지를 호출하기 전 url과 동일해야 합니다. ex) 인증 전 url : http://www.~ 리턴 url : http://www.~
+		$returnurl = "https://dev.ciccommunity.com/checkplus_success";	// 성공시 이동될 URL
+		$errorurl = "https://dev.ciccommunity.com/checkplus_fail";		// 실패시 이동될 URL
+		
+		// reqseq값은 성공페이지로 갈 경우 검증을 위하여 세션에 담아둔다.
+		
+		$_SESSION["REQ_SEQ"] = $reqseq;
+
+		// 입력될 plain 데이타를 만든다.
+		$plaindata = "7:REQ_SEQ" . strlen($reqseq) . ":" . $reqseq .
+					"8:SITECODE" . strlen($sitecode) . ":" . $sitecode .
+					"9:AUTH_TYPE" . strlen($authtype) . ":". $authtype .
+					"7:RTN_URL" . strlen($returnurl) . ":" . $returnurl .
+					"7:ERR_URL" . strlen($errorurl) . ":" . $errorurl .
+					"11:POPUP_GUBUN" . strlen($popgubun) . ":" . $popgubun .
+					"9:CUSTOMIZE" . strlen($customize) . ":" . $customize .
+					"6:GENDER" . strlen($gender) . ":" . $gender ;
+		
+		$enc_data = `$cb_encode_path ENC $sitecode $sitepasswd $plaindata`;
+
+		$returnMsg = "";
+
+		if( $enc_data == -1 )
+		{
+			$returnMsg = "암/복호화 시스템 오류입니다.";
+			$enc_data = "";
+		}
+		else if( $enc_data== -2 )
+		{
+			$returnMsg = "암호화 처리 오류입니다.";
+			$enc_data = "";
+		}
+		else if( $enc_data== -3 )
+		{
+			$returnMsg = "암호화 데이터 오류 입니다.";
+			$enc_data = "";
+		}
+		else if( $enc_data== -9 )
+		{
+			$returnMsg = "입력값 오류 입니다.";
+			$enc_data = "";
 		}
 
-		if($ip == 'UNKNOWN') return true;
-
-		$long_ip = ip2long($ip);
-		if ($ip_list) {
-			foreach ($ip_list as $filter_ip) {
-				$range = explode('-', $filter_ip);
-				if ( ! isset($range[1]) OR empty($range[1])) { // single address type
-					$star_pos = strpos($filter_ip, '*');
-					if ($star_pos !== false) { // wild card exist
-						if (strncmp($filter_ip, $ip, $star_pos) === 0) {
-							return true;
-						}
-					} elseif (strcmp($filter_ip, $ip) === 0) {
-						return true;
-					}
-				} elseif (ip2long($range[0]) <= $long_ip && ip2long($range[1]) >= $long_ip) {
-					return true;
-				}
-			}
-		}
 		return false;
 	}
 
-
-	public function validate($ip_list = array())
-	{
-		/* 사용가능한 표현
-		 * 192.168.2.10 - 4자리의 정확한 ip주소
-		 * 192.168.*.* - 와일드카드(*)가 사용된 4자리의 ip주소, a클래스에는 와일드카드 사용불가,
-		 * 와일드카드 이후의 아이피주소 허용(단, filter()를 쓸 경우 와일드카드 이후 주소는 무시됨
-		 * 192.168.1.1-192.168.1.10 - '-'로 구분된 정확한 4자리의 ip주소 2개
-		 */
-		$regex = "/^
-				(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
-				(?:
-					(?:
-						(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}
-						(?:-(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){1}
-						(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}
-					)
-					|
-					(?:
-						(?:\.(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|\*)){3}
-					)
-				)
-			$/";
-		$regex = str_replace(array("\r\n", "\n", "\r", "\t", " "), '', $regex);
-
-		foreach ($ip_list as $i => $ip) {
-			preg_match($regex, $ip, $matches);
-			if ( ! count($matches)) {
-				return false;
-			}
-		}
-		return true;
-	}
 }
