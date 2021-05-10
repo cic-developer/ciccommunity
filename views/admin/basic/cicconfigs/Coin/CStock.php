f86f53123bcd3d44ea1200b18a22b85d231a2eea
root <root@ip-172-31-42-193.ap-northeast-2.compute.internal>
2021-05-10 00:49:01 +0000

    2021-05-00:05:01 auto commit

--- views/admin/basic/cicconfigs/Coin/CStock.php
+++ views/admin/basic/cicconfigs/Coin/CStock.php
@@ -4,87 +4,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 <div class="box">
-   <!-- Search form (start) -->
-   	<form method='post' action="<?= base_url() ?>index.php/User/loadRecord" >
-     	<input type='text' name='search' value='<?= $search ?>'><input type='submit' name='submit' value='Submit'>
-  	</form>
-   	<br/>
+<!-- Search form (start) -->
+	<form method='post' action="<?= base_url() ?>index.php/User/loadRecord" >
+    	<input type='text' name='search' value='<?= $search ?>'><input type='submit' name='submit' value='Submit'>
+	</form><br/> 
 
+	<!-- Posts List -->
+	<table border='1' width='100%' style='border-collapse: collapse;'>
+		<tr>
+			<th>S.no</th>
+			<th>Title</th>
+			<th>Content</th>
+		</tr>
+		<?php 
+		$sno = $row+1;
+		foreach($result as $data){
 
-	<div class="box-header">
-		<ul class="nav nav-tabs">
-			<li role="presentation" class="active"><a href="<?php echo admin_url($this->pagedir . '/CStock'); ?>" onclick="return check_form_changed();">코인 검색 설정</a></li>
-		</ul>
-	</div>
-	<div class="box-table">
-		<?php
-		echo validation_errors('<div class="alert alert-warning" role="alert">', '</div>');
-		echo show_alert_message(element('alert_message', $view), '<div class="alert alert-auto-close alert-dismissible alert-info"><button type="button" class="close alertclose" >&times;</button>', '</div>');
-		$attributes = array('class' => 'form-horizontal', 'name' => 'fadminwrite', 'id' => 'fadminwrite');
-		echo form_open(current_full_url(), $attributes);
+			$content = substr($data['content'],0, 180)." ...";
+			echo "<tr>";
+			echo "<td>".$sno."</td>";
+			echo "<td><a href='".$data['link']."' target='_blank'>".$data['title']."</a></td>";
+			echo "<td>".$content."</td>";
+			echo "</tr>";
+			$sno++;
+
+		}
+		if(count($result) == 0){
+			echo "<tr>";
+			echo "<td colspan='3'>No record found.</td>";
+			echo "</tr>";
+		}
 		?>
-			<form method ="POST">
-				<input type="hidden" name="is_submit" value="1" />
-				<div class="form-group">
-							활성화 -
-							<label for="cpc_enable" class="control-label">
-								<select name="selected_market" id="doc_layout" class="form-control">
-								<?php
-								foreach($getStock as $stoks){
-											
-									echo '<option value="'.$stoks->name_ko.'">'.$stoks->market.' - '.$stoks->name_ko.' </option>';							
-									}
-								
-								?>	
-								</select>
-							</label>
-				
-						</div>
-						<div class="btn-group pull-right" role="group" aria-label="...">
-							<button type="submit" name="submit" class="btn btn-success btn-sm">저장하기</button>
-						</div>	
-				</div>
+	</table>
 
-			</form>	
+	<!-- Paginate -->
+	<div style='margin-top: 10px;'>
+	<?= $pagination; ?>
+	</div>
 
-            <div class ='box-table'>
-				<div class="box-header">
-						<ul class="nav nav-tabs">
-							<li role="presentation" class="active">update Coin stock</li>
-						</ul>
-				</div>
-				<div class='box-table'>
-					<form>
-							<div class="form-group col-md-2">
-								<label for="inputCity">Market name</label>
-								<input type="text" class="form-control" id="myText" value = '<?php echo $realtime_coin_info['market']; ?>'>
-							</div>
 
-							<div class="form-group col-md-2">
-								<label for="inputCity"> name in Korean</label>
-								<input type="text" class="form-control" id="myText" value = '<?php echo $realtime_coin_info['market']; ?>'>
-							</div>
 
-							<div class="form-group col-md-2">
-								<label for="inputState">high-price</label>
-								<input type="text" class="form-control" id="myText" value = '<?php echo $realtime_coin_info['high_price']; ?>'>
-							</div>
-							<div class="form-group col-md-2">
-								<label for="inputZip">low-price</label>
-								<input type="text" class="form-control" id="myText" value = '<?php echo $realtime_coin_info['low_price']; ?>'>
-							</div>
-							<div class="form-group col-md-2">
-								<label for="inputZip">trade-price</label>
-								<input type="text" class="form-control" id="myText" value = '<?php echo $realtime_coin_info['trade_price']; ?>'>
-							</div>
-							<div class="btn-group pull-right" role="group" aria-label="...">
-								<button type="submit" class="btn btn-success btn-sm">update</button>        
-							</div>
-					</form>
 
-				<?php echo form_close(); ?>
-			</div>
-    
 </div>
 
 <script type="text/javascript">
