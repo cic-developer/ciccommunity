ef72d1b9163326597eeaba46437f02115cc0abea
rsteam-dev <developer@rs-team.com>
2021-05-11 00:45:01 +0000

    2021-05-00:05:01 auto commit

--- application/controllers/admin/cicconfigs/Coin.php
+++ application/controllers/admin/cicconfigs/Coin.php
@@ -24,11 +24,13 @@ class Coin extends CB_Controller
 	/**
 	 * 모델을 로딩합니다
 	 */
-	protected $models = array('Coin');
+	protected $models = array('Coin_model');
 
 	/**
 	 * 이 컨트롤러의 메인 모델 이름입니다
 	 */
+	protected $modelname = 'Coin_model';
+
 	/**
 	 * 헬퍼를 로딩합니다
 	 */
@@ -50,22 +52,22 @@ class Coin extends CB_Controller
 	 */
 	public function CStock()
 	{
+		// 이벤트 라이브러리를 로딩합니다
         $eventname = 'event_stok';
         $this->load->event($eventname);
 		
-		
-		
 		$view = array();
 		$view['view'] = array();
-        $view['view']['event']['before'] = Events::trigger('before', $eventname);
+		
+		// 이벤트가 존재하면 실행합니다
+		$view['view']['event']['before'] = Events::trigger('before', $eventname);
 
 		/**
 		 * 페이지에 숫자가 아닌 문자가 입력되거나 1보다 작은 숫자가 입력되면 에러 페이지를 보여줍니다.
 		 */
 		$param =& $this->querystring;
 		$page = (((int) $this->input->get('page')) > 0) ? ((int) $this->input->get('page')) : 1;
-
-		$findex = $this->input->get('findex') ? $this->input->get('findex') : $this->Coin_model->primary_key;
+		$findex = $this->input->get('findex') ? $this->input->get('findex') : $this->{$this->modelname}->primary_key;
 		$forder = $this->input->get('forder', null, 'desc');
 		$sfield = $this->input->get('sfield', null, '');
 		$skeyword = $this->input->get('skeyword', null, '');
@@ -76,33 +78,42 @@ class Coin extends CB_Controller
 		/**
 		 * 게시판 목록에 필요한 정보를 가져옵니다.
 		 */
-		// 'wid_admin_id', 'wid_admin_ip', 'wid_res_datetime', 'wid_content'
-		// $this->Coin_model->allow_search_field = array(''); // 검색이 가능한 필드
+		$this->{$this->modelname}->allow_search_field = array('mlh_id', 'member_level_history.mem_id', 'member.mem_userid', 'member.mem_nickname', 'mlh_from', 'mlh_to', 'mll_datetime', 'mlh_reason', 'mlh_ip'); // 검색이 가능한 필드
+		$this->{$this->modelname}->search_field_equal = array('mlh_id', 'member_level_history.mem_id', 'mlh_from', 'mlh_to'); // 검색중 like 가 아닌 = 검색을 하는 필드
+		$this->{$this->modelname}->allow_order_field = array('mlh_id'); // 정렬이 가능한 필드
 
-		// $this->Coin_model->search_field_equal = array(''); // 검색중 like 가 아닌 = 검색을 하는 필드
 		
-		// $this->Coin_model->allow_order_field = array(''); // 정렬이 가능한 필드
+		$where = array();
+		if (is_numeric($this->input->get('mlh_from'))) {
+			$where['mlh_from'] = $this->input->get('mlh_from');
+		}
+		if (is_numeric($this->input->get('mlh_to'))) {
+			$where['mlh_to'] = $this->input->get('mlh_to');
 
-		// $where = array();
-		//GET COIN MARKET INFORMATION FOR DROPDOWN LIST
+		}
 
-		$getStock = $this -> Coin_model->getstockData();
+		$result = $this->{$this->modelname}
+		->getstockData($per_page, $offset, $where, '', $findex, $forder, $sfield, $skeyword);
 		$list_num = $result['total_rows'] - ($page - 1) * $per_page;
-		$view['getStock'] = $getStock;
+		if (element('list', $result)) {
+			foreach (element('list', $result) as $key => $val) {
+				$result['list'][$key]['display_name'] = display_username(
+					element('mem_userid', $val),
+					element('mem_nickname', $val),
+					element('mem_icon', $val)
+				);
+				$result['list'][$key]['num'] = $list_num--;
+			}
+		}
+		$view['view']['data'] = $result;
 		
-	
-		/**
-		 * 페이지네이션을 생성합니다
-		 */
-		$config['base_url'] = admin_url($this->pagedir).'?'. $param->replace('page');
-		//$config['total_rows'] = $getStock['total_rows'];
-		$config['per_page'] = $per_page;
-
-		$this->pagination->initialize($config);
-		$view['view']['paging'] = $this->pagination->create_links();
-		$view['view']['page'] = $page;
-		// Initialize
-		$view['view']['getStock'] = $getStock;
+		
+		
+		
+		
+		
+		
+		$view['getStock'] = $getStock;
 		
 		$getList = $this -> Coin_model->get_coinlist();
 		for($i=0; $i<count($getList); $i++){
