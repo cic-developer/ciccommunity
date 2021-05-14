fb2c407bbd569df1fc0b17338699aa9c8b02363b
rsteam-dev <developer@rs-team.com>
2021-05-14 18:30:01 +0900

    2021-05-14_18:30:01 auto commit

--- views/mypage/cic/withdraw.php
+++ views/mypage/cic/withdraw.php
@@ -86,35 +86,33 @@
 </div>
 
 <script>
-    function isNumeric(num, opt = ''){
+    function isNumeric(num){
         // 좌우 trim(공백제거)을 해준다.
         num = String(num).replace(/^\s+|\s+$/g, "");
-        
+        /*
         if(typeof opt == "undefined" || opt == "1"){
-        //     // 모든 10진수 (부호 선택, 자릿수구분기호 선택, 소수점 선택)
-        //     var regex = /^[+\-]?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+){1}(\.[0-9]+)?$/g;
-        // }else if(opt == "2"){
-        //     // 부호 미사용, 자릿수구분기호 선택, 소수점 선택
-        //     var regex = /^(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+){1}(\.[0-9]+)?$/g;
-        // }else if(opt == "3"){
-        //     // 부호 미사용, 자릿수구분기호 미사용, 소수점 선택
-        //     var regex = /^[0-9]+(\.[0-9]+)?$/g;
-        // }else{
-        //     // only 숫자만(부호 미사용, 자릿수구분기호 미사용, 소수점 미사용)
-        //     var regex = /^[0-9]$/g;
-        // }
-        
-        var regex = /^[0-9]+(\.[0-9]+)?$/g;
+            // 모든 10진수 (부호 선택, 자릿수구분기호 선택, 소수점 선택)
+            var regex = /^[+\-]?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+){1}(\.[0-9]+)?$/g;
+        }else if(opt == "2"){
+            // 부호 미사용, 자릿수구분기호 선택, 소수점 선택
+            var regex = /^(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+){1}(\.[0-9]+)?$/g;
+        }else if(opt == "3"){
+            // 부호 미사용, 자릿수구분기호 미사용, 소수점 선택
+            var regex = /^[0-9]+(\.[0-9]+)?$/g;
+        }else{
+            // only 숫자만(부호 미사용, 자릿수구분기호 미사용, 소수점 미사용)
+            var regex = /^[0-9]$/g;
+        }*/
 
+        var regex = /^[0-9]+(\.[0-9]+)?$/g;
+        
         if( regex.test(num) ){
             num = num.replace(/,/g, "");
             return isNaN(num) ? false : true;
-        } else { 
-            return false;  
-        }
+        }else{ return false;  }
     }
 
-                
+
     // 출금금액 validation
     function validateForm() {
         var x, text;
@@ -125,13 +123,7 @@
         btn = document.getElementById("withdraw-request");
 
         // // If x is Not a Number or less than one or greater than 10
-
-        // if(!isNumeric(x)){
-        //     text = "금액을 올바르게 입력해주세요.";
-        //     return;
-        // }
-
-        if (isNaN(x) || x < 1 || x > mem_cp) {
+        if (isNumeric(x) || x < 1 || x > mem_cp) {
             text = "금액을 올바르게 입력해주세요.";
         } else {
             // text = "Input OK";
