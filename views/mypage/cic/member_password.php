187a6e04dea7fb65712269bb33aca04fe2961655
rsteam-dev <developer@rs-team.com>
2021-05-20 20:00:01 +0900

    2021-05-20_20:00:01 auto commit

--- views/mypage/cic/member_password.php
+++ views/mypage/cic/member_password.php
@@ -24,13 +24,28 @@
 				</ul>
 			</div>
 			<div class="lower">
-				<a href="javascript:fconfirmpassword.submit();" class="leave-btn"><span>확인</span></a>
+				<a href="javascript:fconfirmpassword.submit();" class="leave-btn go-btn"><span>확인</span></a>
 				<a href="javascript:history.back();" class="leave-btn"><span>뒤로가기</span></a>
 			</div>
 			<?php echo form_close(); ?>
 		</div>
 	</div>
 </div>
+
+<style>
+/* 보류 */
+.error{
+	background-color:pink;
+	color:black;
+	height:100%;
+	width:100%;
+	z-index:11;
+	position:absolute;
+	display:block;
+	clear:both;
+	display:block;
+}
+</style>
 
 
 <script type="text/javascript">
@@ -46,4 +61,13 @@
 		});
 	});
 	//]]>
+
+	$(document).on('click', '.go-btn',function(){
+		var password = $('#mem_password').val();
+	
+
+		// if((password.trim()).length < 4){
+		// 	alert('비밀번호를 4자 이상 입력해주세요');
+		// }
+	})
 </script>
