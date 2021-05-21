80aaa33f0da35bbbd6069e72a8722f31760efafc
rsteam-dev <developer@rs-team.com>
2021-05-21 09:15:01 +0900

    2021-05-21_09:15:01 auto commit

--- views/search/cic/search.php
+++ views/search/cic/search.php
@@ -49,6 +49,9 @@
 				<div class="result list txt content-heading" style="height: 170px;">
 					<div class="txt" >
 						<div class="vc" style="float:left">
+							<div>
+								<canvas id="canvas"></canvas>
+							</div>
 							<p class="stxt pimg"><img src="https://static.upbit.com/logos/<?php echo $symbole; ?>.png"></img></p>
 							<p class="btxt center"> <strong><?php echo $korean; ?></strong></span> </p>
 						</div><br>
@@ -302,3 +305,40 @@
 	</div>
 	<!-- e: #container-wrap //-->
 </div>
+<script>
+var ctx = document.getElementById('canvas').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script>
