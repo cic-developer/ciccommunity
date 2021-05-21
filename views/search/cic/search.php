b0865f435ac4c4dc93e29fadf60f1076184dfd4b
rsteam-dev <developer@rs-team.com>
2021-05-21 22:45:01 +0900

    2021-05-21_22:45:01 auto commit

--- views/search/cic/search.php
+++ views/search/cic/search.php
@@ -55,7 +55,7 @@
 			<!-- HERE THE PRICE -->
 			<?php if($trade){ ?>
 				<div class="result" style="height: 180px">
-					<div style="border-bottom: 1px solid #aaa;">
+					<div style="border-bottom: 1px solid #aaa; border-radius: 25px;">
 						<br><img style= "width: 30px" src="https://static.upbit.com/logos/<?php echo $symbole; ?>.png"></img>
 						<strong  class="btxt"><?php echo $korean; ?></strong>
 						<p class="stxt"><span> KRW-<?php echo $symbole; ?> </span></p>
