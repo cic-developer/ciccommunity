98740aec4ee19ec26d1a597409fc580406619024
rsteam-dev <developer@rs-team.com>
2021-05-21 11:45:01 +0900

    2021-05-21_11:45:01 auto commit

--- views/mypage/cic/member_password.php
+++ views/mypage/cic/member_password.php
@@ -7,6 +7,7 @@
 		<div class="member-wrap modify">
 
 		<?php
+			echo validation_errors('<script>alert("', '");</script>');
 			echo show_alert_message($this->session->flashdata('message'), '<script>alert("', '");</script>');
 			$attributes = array('class' => 'form-horizontal', 'name' => 'fconfirmpassword', 'id' => 'fconfirmpassword');
 			echo form_open(current_url(), $attributes);
@@ -19,14 +20,13 @@
 						<div class="field">
 							<p class="chk-input w210">
 								<input id="mem_password" name="mem_password" type="password" placeholder="비밀번호 입력" value="">
-								<?php echo validation_errors('<div class="alert alert-warning" role="alert">', '</div>'); ?>
 							</p>
 						</div>
 					</li>
 				</ul>
 			</div>
 			<div class="lower">
-				<button type="submit" class="leave-btn go-btn"><span>확인</span></button>
+				<button type="submit" class="update-btn go-btn"><span>확인</span></button>
 				<button type="button" class="leave-btn"><span>뒤로가기</span></button>
 			</div>
 			<?php echo form_close(); ?>
@@ -34,26 +34,64 @@
 	</div>
 </div>
 
-<style>
-/* 보류 : ciboard error가 보이지 않는 부분 style은 문의해보기 (memo) */
-.error{
-	background-color:pink;
-	color:black;
-	height:100%;
-	width:100%;
-	z-index:11;
-	position:absolute;
-	display:block;
-	clear:both;
-	display:block;
-}
-</style>
-
 
 <script type="text/javascript">
-	$(document).on('click', '.leave-btn', function(){
-		history.back();
-	});
+	// $.validator.setDefaults({
+	// 	onkeyup:false,
+	// 	onclick:false,
+	// 	onfocusout:false,
+	// 	showErrors:function(errorMap, errorList){
+	// 		if(this.numberOfInvalids()) {
+	// 			alert(errorList[0].message);
+	// 		}
+	// 	}
+	// });
+
+	$.validator.setDefaults({
+
+showErrors: function(map, list) {
+
+		// there's probably a way to simplify this
+
+		var focussed = document.activeElement;
+
+		if (focussed && $(focussed).is("input, textarea")) {
+
+			   $(this.currentForm).tooltip("close", {
+
+					   currentTarget: focussed
+
+			   }, true)
+
+		}
+
+		var className = "ui-state-highlight"; //error시 변경될 class
+
+	   
+
+		this.currentElements.removeAttr("title").removeClass(className);
+
+		$.each(list, function(index, error) {
+
+			   $(error.element).attr("title", error.message).addClass(className);
+
+		});
+
+		/* tooltip 자동 open
+
+		if (focussed && $(focussed).is("input, textarea")) {
+
+			   $(this.currentForm).tooltip("open", {
+
+					   target: focussed
+
+			   });
+
+		}*/
+
+},
+
+
 
 	//<![CDATA[
 	$(function () {
