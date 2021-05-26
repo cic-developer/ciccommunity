ca9e5f8d0ab6f045d14d4eeb0644e416c01a0c32
rsteam-dev <developer@rs-team.com>
2021-05-26 10:00:01 +0900

    2021-05-26_10:00:01 auto commit

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -177,10 +177,10 @@ class Search extends CB_Controller
 		$total_rows = $result['total_rows'];
 		$view['total_rows'] = $total_rows;
 
-		echo "<pre><br>";
-		// print_r($grouplist);
-		print_r($boardlist);
-		echo "</pre></br>";
+		// echo "<pre><br>";
+		// // print_r($grouplist);
+		// print_r($boardlist);
+		// echo "</pre></br>";
 
 		if ( ! $this->session->userdata('skeyword_' . urlencode($skeyword))) {
 			$sfieldarray = array('post_title', 'post_content', 'post_both');
