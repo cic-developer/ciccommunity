aacbd5df04208c63a1a515e638dbd78411430afd
rsteam-dev <developer@rs-team.com>
2021-05-28 09:00:02 +0900

    2021-05-28_09:00:01 auto commit

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -18,7 +18,7 @@ class News extends CB_Controller
 	/**
 	 * 모델을 로딩합니다
 	 */
-	protected $models = array('Post', 'Post_meta', 'Post_extra_vars', 'CIC_member_level_config');
+	protected $models = array('News');
 
 	/**
 	 * 헬퍼를 로딩합니다
@@ -41,32 +41,10 @@ class News extends CB_Controller
 	 */
 	public function index()
 	{
-        $page_title = $this->cbconfig->item('site_meta_title_main');
-		$meta_description = $this->cbconfig->item('site_meta_description_main');
-		$meta_keywords = $this->cbconfig->item('site_meta_keywords_main');
-		$meta_author = $this->cbconfig->item('site_meta_author_main');
-		$page_name = $this->cbconfig->item('site_page_name_main');
-        
-		$layoutconfig = array(
-			'path' => 'news',
-			'layout' => 'layout',
-			'skin' => 'news',
-			// 'layout_dir' => $this->cbconfig->item('layout_main'),
-			'layout_dir' => 'cic_sub',
-			'mobile_layout_dir' =>'cic_sub',
-			'use_sidebar' => $this->cbconfig->item('sidebar_main'),
-			'use_mobile_sidebar' => $this->cbconfig->item('mobile_sidebar_main'),
-			'skin_dir' => $this->cbconfig->item('skin_main'),
-			'mobile_skin_dir' => $this->cbconfig->item('mobile_skin_main'),
-			'page_title' => $page_title,
-			'meta_description' => $meta_description,
-			'meta_keywords' => $meta_keywords,
-			'meta_author' => $meta_author,
-			'page_name' => $page_name,
-		);
-		$view['layout'] = $this->managelayout->front($layoutconfig, $this->cbconfig->get_device_view_type());
-		$this->data = $view;
-		$this->layout = element('layout_skin_file', element('layout', $view));
-		$this->view = element('view_skin_file', element('layout', $view));
-    }
+		$eventname = 'event_news_post_list';
+		$this->load->event($eventname);
+
+		
+		
+	}
 }
