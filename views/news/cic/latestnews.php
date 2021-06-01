f1be4e13987ae44c76dff13201c13d9d122dbbb6
rsteam-dev <developer@rs-team.com>
2021-06-01 18:15:01 +0900

    2021-06-01_18:15:01 auto commit

--- views/news/cic/latestnews.php
+++ views/news/cic/latestnews.php
@@ -107,7 +107,7 @@
 							foreach (element('list', element('data', $view)) as $result) {
 								?>
 							<li>
-								<a href="<?php echo goto_url(element('newsurl', $result)); ?>" target="_blank">
+								<a href="<?php echo site_url('/news/news_url/'.element('news_id', $result)); ?>">
 									<div class="img"><img src="<?php echo html_escape(element('news_image', $result)) ?>" alt="">
 									</div>
 									<div class="txt">
