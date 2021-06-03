06c12f1a25d81d034d9f50bbc7e896b8ba735377
rsteam-dev <developer@rs-team.com>
2021-06-03 15:00:01 +0900

    2021-06-03_15:00:01 auto commit

--- application/models/CIC_forum_model.php
+++ application/models/CIC_forum_model.php
@@ -191,13 +191,12 @@ class CIC_forum_model extends CB_Model
 
 		$this->db->select('cic_forum_info.*');
 		$this->db->select_sum('cic_forum_cp.cfc_cp', 'cic_forum_total_cp');
-		// $this->db->select_sum('CASE WHEN  `cic_forum_cp`.`cfc_option` = 1 THEN `cic_forum_cp`.`cfc_cp` ELSE 0 END', 'cic_A_cp');
+		$this->db->select('CASE WHEN  `cic_forum_cp`.`cfc_option` = 1 THEN `cic_forum_cp`.`cfc_cp` ELSE 0 END', 'cic_A_cp');
 		// $this->db->select_sum('CASE WHEN  `cic_forum_cp`.`cfc_option` = 2 THEN `cic_forum_cp`.`cfc_cp` ELSE 0 END', 'cic_B_cp');
 
 		// $this->db->from('$this->_table');
 		$this->db->from('cic_forum_info');
 		$this->db->join('cic_forum_cp', 'cic_forum_info.pst_id = cic_forum_cp.pst_id', 'left');
-		// $this->db>query('hihihihihi@@@@');
 
 		if ($primary_value) {
 			// $this->db->where($this->primary_key, $primary_value);
