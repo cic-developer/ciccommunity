fab2c312b33d30770ff16383fe34ba0a64a28b98
rsteam-dev <developer@rs-team.com>
2021-06-04 14:15:02 +0900

    2021-06-04_14:15:01 auto commit

--- views/admin/basic/cicconfigs/forum/forum_write copy.php
+++ views/admin/basic/cicconfigs/forum/forum_write copy.php
@@ -1,9 +1,4 @@
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
+
 <div class="box">
 	<div class="box-table">
 		<?php
@@ -36,6 +31,24 @@
 					<p class="help-block">배팅 마감일은 포럼 마감일 이전으로 설정해야합니다.</p>
 				</div>
 			</div>
+			
+					
+			<!-- <div class="form-group">
+				<label class="col-sm-2 control-label">배팅 마감일</label>
+				<div class='input-group date' id='datetimepicker1'>
+					<input type='text' class="form-control" value="<?php echo set_value('frm_close_datetime', element('frm_close_datetime', element('data', $view))); ?>"/>
+					<p class="help-block">배팅 마감일은 포럼 마감일 이전으로 설정해야합니다.</p>
+					<span class="input-group-addon">
+						<span class="glyphicon glyphicon-calendar"></span>
+					</span>
+				</div>
+			</div>
+				<script type="text/javascript">
+					$(function () {
+						$('#datetimepicker1').datetimepicker();
+					});
+				</script>
+			</div> -->
 			<div class="form-group">
 				<label class="col-sm-2 control-label">포럼 마감일</label>
 				<div class="col-sm-10 form-inline" id="datetimepicker1">
