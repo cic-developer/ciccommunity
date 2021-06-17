569b5c51c04b7e547b8520b7ce28d72581d3bc9f
rsteam-dev <developer@rs-team.com>
2021-06-17 18:30:01 +0900

    2021-06-17_18:30:01 auto commit

--- views/board/cic_writer/list.php
+++ views/board/cic_writer/list.php
@@ -93,12 +93,12 @@
                                     </p>
                                 </div>
                             </div>
-                            </a>
+                        </a>
                             <div class="abr">
-                                <div class="photo" onclick="alert('asdasdasdasd')">
+                                <div class="photo">
                                     <p class="pimg"><img src="<?php echo thumb_url('mlc_attach', element('mlc_attach', $result), 30, 30);?>"
                                             alt="<?php echo element('mlc_title', $result); ?>"></p>
-                                    <p class="rtxt"><?php echo element('post_nickname', $result); ?></p>
+                                    <p class="rtxt popup_menuu"><?php echo element('post_nickname', $result); ?></p>
                                 </div>
                                 <p class="vp"><?php echo number_format(element('post_like_point', $result)-element('post_dislike_point', $result)); ?> VP</p>
                             </div>
@@ -156,3 +156,42 @@
         </div>
         <!— page end // —>
     </div>
+    <!-- 작성 글 보기-->
+<div class="popupLayer" style="display:none; z-index:1200">
+	<a href="" class="layer_link"> 작성 글 보기</a>
+</div>
+<script>
+    $(function(){
+        /* 클릭 클릭시 클릭을 클릭한 위치 근처에 레이어가 나타난다. */
+        $('.popup_menuu').click(function(e)
+        {
+            var sWidth = window.innerWidth;
+            var sHeight = window.innerHeight;
+
+            var oWidth = $('.popupLayer').width();
+            var oHeight = $('.popupLayer').height();
+
+            // 레이어가 나타날 위치를 셋팅한다.
+            var divLeft = e.clientX + 10;
+            var divTop = e.clientY + 5;
+
+            // 레이어 위치를 바꾸었더니 상단기준점(0,0) 밖으로 벗어난다면 상단기준점(0,0)에 배치하자.
+            if( divLeft < 0 ) divLeft = 0;
+            if( divTop < 0 ) divTop = 0;
+            
+            // $('.layer_link').prop('href', 'https://dev.ciccommunity.com/board/freetalk?sfield=post_nickname&skeyword='+ $(this).text() +'&search_submit=');
+            $('.layer_link').prop('href', `<?php echo base_url('board/cicwriter').'?sfield=post_nickname&skeyword='?>${$(this).text()}`);
+            $('.popupLayer').css({
+                "top": divTop,
+                "left": divLeft,
+                "display" : "block"
+            }).show();
+        });
+    });
+    $(document).mouseup(function (e){
+        var container = $('.popupLayer');
+        if( container.has(e.target).length === 0){
+        container.css('display','none');
+        }
+    });
+</script>
