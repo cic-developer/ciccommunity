5c923572d6e868c6e229e44c7e76e00abbf8a2c9
rsteam-dev <developer@rs-team.com>
2021-06-22 17:30:01 +0900

    2021-06-22_17:30:01 auto commit

--- views/admin/basic/service/attendancecfg/list.php
+++ views/admin/basic/service/attendancecfg/list.php
@@ -19,6 +19,9 @@
 		?>
         <div class="row">전체 : <?php echo element('total_rows', element('data', $view), 0); ?>건</div>
         <div class="table-responsive">
+			<pre>
+			<?php echo element('list', element('data', $view))?>
+			</pre>
 			<table class="table table-hover table-striped table-bordered">
             <thead>
 				<tr>
@@ -32,7 +35,7 @@
                 </tr>
             </thead>
                 <tbody>
-                <?php if ( ! element('list', element('data', $view))) {?>
+					<?php if ( ! element('list', element('data', $view))) {?>
 						<tr>
 							<td colspan="17" class="nopost">자료가 없습니다</td>
 						</tr>
