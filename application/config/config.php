26fd4fd997314ad42a2de129b872c7f52f38e7c0
rsteam-dev <developer@rs-team.com>
2021-06-24 15:45:01 +0900

    2021-06-24_15:45:01 auto commit

--- application/config/config.php
+++ application/config/config.php
@@ -492,7 +492,7 @@ $config['encryption_key'] = 'EjrqhRdlaktlWudwlsWkwlsWkaktlTdjd';
  */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 18000;
 //$config['sess_save_path'] =  APPPATH.'cache/session/';
 $config['sess_save_path'] =  NULL;
 $config['sess_match_ip'] = FALSE;
